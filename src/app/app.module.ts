d166827a4420c9ad6df5236763111c36f11bec63
bubuStyle <bubustyle4@gmail.com>
2017-02-14 12:58:35 +0100

    Ajout de 2 nouveaux composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,44 @@
+// Importer les class pour configurer le module :
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Importer le module des routes :
+import { Routing } from './routing';
+
+// Importer le composant principal :
 import { AppComponent } from './app.component';
+
+// Importer les composants de l'application (page) :
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AddStudentComponent } from './components/add-student/add-student.component';
 import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { HeaderComponent } from './directives/header/header.component';
+import { FooterComponent } from './directives/footer/footer.component';
 
 @NgModule({
+  // Ajouter tous les composants dans le tableau des déclarations :
   declarations: [
     AppComponent,
     DashboardComponent,
     AddStudentComponent,
-    EditStudentComponent
+    EditStudentComponent,
+    HeaderComponent,
+    FooterComponent
   ],
+
+  // Ajouter les modules dans le tableau des imports :
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    // Ajouter la class des routes :
+    Routing
   ],
   providers: [],
+
+  // Ajouter le composant principal dans le tableau bootstrap :
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/routing.ts
@@ -0,0 +1,45 @@
+// Importer les class nécessaires à l'utilisation des routes :
+import { ModuleWithProviders } from '@angular/core'; // En UpperCamelCase
+import { Routes, RouterModule } from '@angular/router';
+
+// Importer les composants à utiliser dans les routes :
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+
+// Déclarer une constante de type Routes = variable non modifiable :
+const appRoutes: Routes = [
+
+    // Définition des routes :
+    {
+        // Redirection de la page d'accueil :
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+
+    {
+        // Définir l'adresse :
+        path: 'dashboard',
+        // Définir le composants à utiliser :
+        component: DashboardComponent
+    },
+
+    {
+        // Définir l'adresse :
+        path: 'add-student',
+        // Définir le composants à utiliser :
+        component: AddStudentComponent
+    },
+
+    {
+        // Définir l'adresse :
+        path: 'edit-student',
+        // Définir le composants à utiliser :
+        component: EditStudentComponent
+    }
+
+];
+
+// Exporter la constante des routes :
+export const Routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
