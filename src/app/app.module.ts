b56660cd06ec0945dcf4f5e6291f82ce0ee933ea
bubuStyle <bubustyle4@gmail.com>
2017-02-14 10:52:53 +0100

    Ajout de 3 nouveaux composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddStudentComponent,
+    EditStudentComponent
   ],
   imports: [
     BrowserModule,
