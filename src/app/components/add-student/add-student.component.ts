f2b241e22da5abf361ded09ed69ff4fd9e8c46fe
bubuStyle <bubustyle4@gmail.com>
2017-02-15 15:16:56 +0100

    Rajout du module d'ID

--- src/app/components/add-student/add-student.component.ts
+++ src/app/components/add-student/add-student.component.ts
@@ -1,14 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 
+// 1) Importer la class du service :
+import { StudentService } from '../../services/student.service';
+
 @Component({
   selector: 'app-add-student',
-  templateUrl: './add-student.component.html'
+  templateUrl: './add-student.component.html',
+
+  // 2) Ajouter le service dans le tableau des providers :
+  providers: [StudentService]
+
 })
 export class AddStudentComponent implements OnInit {
 
-  constructor() { }
+  // Créer un objet vide qui reprend la structure des objets du service :
+  newStudent = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    state: 0 // Le type number ne peut pas être vide
+  };
+
+  // Créer une variable pour récup la taille de la liste d'étudiants :
+  myListLength;
+
+  // 3) Définir une variable pour utiliser le service :
+  constructor(
+    private studentService: StudentService
+  ) {}
+
+  // Créer une fonction pour vider le formulaire :
+  resetInput(){
+    // Annuler la modification de l'objet par l'utilisateur :
+    this.newStudent = {
+      id: 0,
+      firstName: '',
+      lastName: '',
+      state: 0 // Le type number ne peut pas être vide
+    };
+  }
+
+  // Créer une fonction pour ajouter un étudiant dans la liste du service :
+  addNewStudent(object){
+    // Appeler la fonction du service en lui envoyant un nouvel objet :
+    this.studentService.addStudentInStudentList(object);
+
+    // Vider le formulaire :
+    this.resetInput();
+
+    // Actualiser la valeur de la taille de tableau LIST :
+    this.getNextStudentId();
+  };
+
+  // Créer une fonction pour connaitre la taille du tableau LIST du service :
+  getNextStudentId(){
+    this.studentService.getListLength().then( number => this.newStudent.id = number );
+  };
 
   ngOnInit() {
-  }
+    // Connaitre la taille du tableau LIST au chargement du composant :
+    this.getNextStudentId();
+  };
 
 }
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -10,21 +10,25 @@ import 'rxjs/add/operator/toPromise';
 // Créer et exporter une collection de données :
 export const LIST = [
   {
+    id: 0,
     firstName: 'Georges',
     lastName: 'Harrison',
     state: 1
   },
   {
+    id: 1,
     firstName: 'Paul',
     lastName: 'McCartney',
     state: 0
   },
   {
+    id: 2,
     firstName: 'John',
     lastName: 'Lennon',
     state: 2
   },
   {
+    id: 3,
     firstName: 'Ringo',
     lastName: 'Starr',
     state: 0
@@ -50,4 +54,17 @@ export class StudentService {
 
   }
 
-}
+  // Créer une fonction pour ajouter un étudiant dans la liste :
+  addStudentInStudentList(object){
+
+    // Ajouter le nouvel étudiant dans le tableau :
+    LIST.push(object);
+
+  };
+
+  // Créer une fonction pour connaitre la taille du tableau LIST :
+  getListLength(): Promise<number>{
+    return Promise.resolve(LIST.length);
+  };
+
+};
